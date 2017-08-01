2f2108852394294fdf78d76a58763264b4227205
Delveri Chick <chickdelveri@gmail.com>
2017-08-01 09:21:29 -0400

    Encode code digest in tryit url

--- routes/codes.js
+++ routes/codes.js
@@ -30,4 +30,41 @@ router.get('/', function(req, res, next) {
   // res.send(JSON.stringify({name : " Chidelmun", email : "root@localhost.com"}));
 });
 
+
+router.get('/api/:digest', function(req,res,next){
+	var digest = req.params.digest;
+
+	var connection = mysql.createConnection({
+  	user : 'root',
+  	host : 'localhost',
+  	password : 'root',
+  	database : 'Quak'
+
+  });
+
+	  connection.connect(function(err){
+  	if (err) {
+  		console.log("Error connecting to Database");
+  	}else{
+  		console.log("Connection Successful");
+  	}
+  });
+
+
+  connection.query("select * from codes where digest = ? ", [digest], function(error, results){
+  	if (error) {
+  		console.log("Error with Query :" + error);
+  	}else{
+  		if (results.length == 0) {
+  			console.log("No Digest Found");
+  			res.send("No Digest Found");
+  		}else{
+  		res.json(results[0].digest);	
+  		}
+  		
+  	}
+  });
+
+	
+});
 module.exports = router;
