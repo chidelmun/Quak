aa7608472fb79b138c3c90dfcbb17c5493b52225
Delveri Chick <chickdelveri@gmail.com>
2017-08-02 11:58:21 -0400

    Generate correct URL

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quak Playground' });
+  res.render('index', { title: 'Quak Playground', url : req.protocol + '://' + req.get('host') + req.originalUrl });
 });
 
 module.exports = router;
